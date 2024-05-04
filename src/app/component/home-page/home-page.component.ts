a2b7fbf66c138d786c93d4d27ce00a9ccfb61899
poonam-kumawat <mailpoonam2002@gmail.com>
2024-05-05 00:00:15 +0530

    added some css

--- src/app/component/home-page/home-page.component.ts
+++ src/app/component/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, Inject, OnDestroy, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, HostListener, Inject, OnDestroy, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import AOS from 'aos';
 // import * as AOS from 'aos';
 import 'aos/dist/aos.css';
 import { NgxTypedJsComponent } from 'ngx-typed-js';
+import { Subject, debounceTime } from 'rxjs';
 // import { Typewriter } from 'typewriter-effect';
 
 
@@ -14,63 +15,66 @@ import { NgxTypedJsComponent } from 'ngx-typed-js';
 })
 export class HomePageComponent implements OnInit {
   typedStrings: string[] = [
-    `       <p class="text-[#EEFF31] text-xs font-mono"><span class="text-[#5918DF]">class </span> PoonamKumawat<span
-                class="text-[#ffffff]"> &#123;</span></p>
-        <p class="text-[#ffffff] text-xs font-mono"><span class="text-[#5918DF]">constructor </span>&#40;&#41;<span
-                class="text-[#ffffff]"> &#123;</span></p>
-        <p class="text-[#E3244C]  text-xsfont-mono">this<span class="text-[#24E394]">.name</span><span
-                class="text-[#E3244C]">&#61;</span><span class="text-[#24E394]">"Poonam Kumawat"</span><span
-                class="text-[#ffffff]">&#59;</span></p>
-        <p class="text-[#E3244C] text-xs font-mono">this<span class="text-[#24E394]">.dateOfBirth</span><span
-                class="text-[#E3244C]">&#61;</span><span class="text-[#24E394]">"05012002"</span><span
-                class="text-[#ffffff]">&#59;</span></p>
-        <p class="text-[#E3244C] text-xs font-mono">this<span class="text-[#24E394]">.email</span><span
-                class="text-[#E3244C]">&#61;</span><span
-                class="text-[#24E394]">"mailpoonam2002&#64;gmail.com"</span><span class="text-[#ffffff]">&#59;</span>
+    `       <p style="color:#EEFF31"><span style="color:#5918DF">class </span> PoonamKumawat<span
+                style="color:#ffffff"> &#123;</span></p>
+        <p style="color:#ffffff"><span style="color:#5918DF">constructor </span>&#40;&#41;<span
+                style="color:#ffffff"> &#123;</span></p>
+        <p style="color:#E3244C">this<span style="color:#24E394">.name</span><span
+                style="color:#E3244C">&#61;</span><span style="color:#24E394">"Poonam Kumawat"</span><span
+                style="color:#ffffff">&#59;</span></p>
+        <p style="color:#E3244C">this<span style="color:#24E394">.dateOfBirth</span><span
+                style="color:#E3244C">&#61;</span><span style="color:#24E394">"05012002"</span><span
+                style="color:#ffffff">&#59;</span></p>
+        <p style="color:#E3244C">this<span style="color:#24E394">.email</span><span
+                style="color:#E3244C">&#61;</span><span
+                style="color:#24E394">"mailpoonam2002&#64;gmail.com"</span><span style="color:#ffffff">&#59;</span>
         </p>
-        <p class="text-[#ffffff] text-xs font-mono">&#125;</p>
-        <p class="text-[#EEFF31] text-xs font-mono">workExperince<span class="text-[#ffffff]"> &#123;</span></p>
-        <p class="text-[#ffffff] text-xs font-mono"><span class="text-[#5918DF]">return </span>&#91;</p>
-        <p class="text-[#ffffff] text-xs  font-mono">&#123;<span class="text-[#24E394]">'2023 : Present'</span> : <span
-                class="text-[#24E394]">'Junior Software Engineer'</span>&#125;&#59;</p>
-        <p class="text-[#ffffff] text-xs font-mono">&#93;</p>
-        <p class="text-[#ffffff] text-xs font-mono">&#125;</p>
-        <!--  -->
-        <p class="text-[#EEFF31] text-xs font-mono">education<span class="text-[#ffffff]"> &#123;</span></p>
-        <p class="text-[#ffffff] text-xs font-mono"><span class="text-[#5918DF]">return </span>&#91;</p>
-        <p class="text-[#ffffff] text-xs font-mono">&#123;<span class="text-[#24E394]">'2021 : 2023'</span> : <span
-                class="text-[#24E394]">'Mumbai University- Master in Computer Applications'</span>&#125;,</p>
-        <p class="text-[#ffffff] text-xs font-mono">&#123;<span class="text-[#24E394]">'2028 : 2021'</span> : <span
-                class="text-[#24E394]">'Mumbai University- Bachelor in Information Technology'</span>&#125;,</p>
-        <p class="text-[#ffffff]  font-mono">&#93;</p>
-        <p class="text-[#ffffff]  font-mono">&#125;</p>
-        <!--  -->
-        <p class="text-[#EEFF31] text-xs font-mono">skills<span class="text-[#ffffff]"> &#123;</span></p>
-        <p class="text-[#ffffff] text-xs font-mono"><span class="text-[#5918DF]">return </span>&#91;</p>
-        <p class="text-[#ffffff] text-xs font-mono"><span
-                class="text-[#24E394]">'HTML/CSS/JS','Angular','React','Nextjs',<br>'Bootstrap/Tailwind CSS',
+        <p style="color:#ffffff">&#125;</p>
+        <p style="color:#EEFF31">workExperince<span style="color:#ffffff"> &#123;</span></p>
+        <p style="color:#ffffff"><span style="color:#5918DF">return </span>&#91;</p>
+        <p style="color:#ffffff">&#123;<span style="color:#24E394">'2023 : Present'</span> : <span
+                style="color:#24E394">'Junior Software Engineer'</span>&#125;&#59;</p>
+        <p style="color:#ffffff">&#93;</p>
+        <p style="color:#ffffff">&#125;</p>   
+        <p style="color:#EEFF31">education<span style="color:#ffffff"> &#123;</span></p>
+        <p style="color:#ffffff"><span style="color:#5918DF">return </span>&#91;</p>
+        <p style="color:#ffffff">&#123;<span style="color:#24E394">'2021 : 2023'</span> : <span
+                style="color:#24E394">'Mumbai University- Master in Computer Applications'</span>&#125;,</p>
+        <p style="color:#ffffff">&#123;<span style="color:#24E394">'2028 : 2021'</span> : <span
+                style="color:#24E394">'Mumbai University- Bachelor in Information Technology'</span>&#125;,</p>
+        <p style="color:#ffffff">&#93;</p>
+        <p style="color:#ffffff">&#125;</p>
+          <p style="color:#EEFF31">skills<span style="color:#ffffff"> &#123;</span></p>
+        <p style="color:#ffffff"><span style="color:#5918DF">return </span>&#91;</p>
+        <p style="color:#ffffff"><span
+                style="color:#24E394">'HTML/CSS/JS','Angular','React','Nextjs',<br>'Bootstrap/Tailwind CSS',
                 'Typescript', 'Git/Github'</span>
             &#59;</p>
-        <p class="text-[#ffffff] text-xs font-mono">&#93;</p>
-        <p class="text-[#ffffff] text-xs font-mono">&#125;</p>
-        <p class="text-[#ffffff] text-xs font-mono">&#125;</p>`,
+        <p style="color:#ffffff">&#93;</p>
+        <p style="color:#ffffff">&#125;</p>
+        <p style="color:#ffffff">&#125;</p>`,
   ];
   isBrowser: boolean;
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-     this.isBrowser = isPlatformBrowser(this.platformId);
+    this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
   cssLoader = true;
-
+  private scrollEvent = new Subject<void>();
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       AOS.init();
+      
     }
-    AOS.refresh();
 
     setTimeout(() => {
       this.cssLoader = false;
     }, 2000);
+    // AOS.refresh();
+  }
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+      AOS.refresh();
   }
 
   ismobilenav = false;
